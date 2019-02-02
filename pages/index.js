a05d4626b4a28dc6953a5e5a8dfed3d1a5fc447f
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2019-02-02 15:50:01 +0530

    Added nextjs

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Index = () => {
+ return (
+     <div>
+         <p>Hello Next.js, this is your friend Brian from logrocket</p>
+     </div>
+ );
+};
+export default Index
