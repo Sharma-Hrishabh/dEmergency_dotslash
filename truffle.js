48e478dffb198e00020e578e3df1824aa2c0c327
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-02-02 23:24:40 +0530

    rinkeby deploy

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
+
+
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 
@@ -7,6 +11,10 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '5777'
+    },
+    rinkeby:{
+      provider: () => new HDWalletProvider(process.env.MNEMONIC,"https://rinkeby.infura.io/v3/"+process.env.INFURA_API_KEY),
+      network_id: 4
     }
   }
 }
