b2d51fc8dd277e89973fb9f3d40f8443d1f6c7b3
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2019-02-03 10:54:47 +0530

    Added ui of discharge

--- pages/discharge.js
+++ pages/discharge.js
@@ -1,15 +1,18 @@
 import React, { Component }from 'react';
-import {Button, Icon, Container, Dimmer, Loader} from 'semantic-ui-react'
+import {Button, Icon, Container, Dimmer, Loader, Segment, TransitionablePortal, Header, Form} from 'semantic-ui-react'
 import Layout from '../common/Layout';
-import Header from '../common/Header';
 import { Link } from '../routes';
-class Admin extends Component{
+import App from '../connection/app';
+
+
+class Discharge extends Component{
 
   state={
       patientAddress:'',
       open:false,
       loadingPage: true,
-      loading: false
+      loading: false,
+      load: false
   };
 
     componentDidMount() {
@@ -17,10 +20,24 @@ class Admin extends Component{
     }
 
     onSubmit = ()=>{
-      var patientAddress = this.state.patient
+        this.setState({load: true});
+      var patientAddress = this.state.patientAddress;
       App.discharge(patientAddress);
+        this.setState({load: false});
     };
 
+    handleClose= () => {
+        this.setState({open: false});
+        this.setState({loading: false});
+    };
+
+    handleOpen = () => {
+        this.setState({open: true});
+        this.setState({loading: false})
+    };
+    renderDischarge = () => {
+        this.setState({loading: true});
+    };
     render() {
         if(this.state.loadingPage){
             return(
@@ -33,19 +50,56 @@ class Admin extends Component{
         }
         return (
             <Layout>
-                <div style={{display: 'flex', justifyContent: 'flex-end', marginTop: 30}}>
-                    <Link route="/addhospitals">
-                        <Button
-                            primary
-                            size='big'
-                            loading={false}
-                        >
-                            Discharge
-                        </Button>
-                    </Link>
-                </div>
+                    <TransitionablePortal
+                        open={this.state.open}
+                        onOpen={this.handleOpen}
+                        onClose={this.handleClose}
+                        openOnTriggerClick
+                        trigger={
+                            <div style={{display: 'flex', justifyContent: 'flex-end', marginTop: 30}}>
+                            <Button
+                                primary
+                                size='big'
+                                loading={this.state.loading}
+                                onClick={() => {this.setState({open: true})}}
+                            >
+                                Discharge Patient
+                            </Button>
+                            </div>
+                        }
+                    >
+                        <Segment style={{ left: '35%', width: 500 ,position: 'fixed', top: '30%', zIndex: 1000 }}>
+                            {this.renderDischarge}
+                            <div>
+                                <Header>Enter Patient details</Header>
+                                <Form >
+                                    <Form.Field >
+                                        <input type='text' focus="true" placeholder='Patient Address' onChange={(e) => {this.setState({patientAddress: e.target.value})}}/>
+                                    </Form.Field>
+                                    <Form.Field >
+                                        <Button
+                                            negative
+                                            style={{ flex: 1, justifyContent: 'flex-end'}}
+                                            onClick={this.handleClose}
+                                        >
+                                            Cancel
+                                        </Button>
+                                        <Button
+                                            loading={this.state.load}
+                                            style={{ background: '#3fa53f', color:'#ffffff',flex: 1, justifyContent: 'flex-end'}}
+                                            onClick={this.onSubmit}
+                                        >
+                                            Discharge
+                                        </Button>
+                                    </Form.Field>
+                                </Form>
+                            </div>
+                        </Segment>
+                    </TransitionablePortal>
             </Layout>
         );
     }
-};
-export default Admin
+
+
+}
+export default Discharge;
