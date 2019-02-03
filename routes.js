d3433928d46974efc52139d9de12fae0a61ab44a
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2019-02-03 07:54:27 +0530

    added discharge page

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const routes = require('next-routes')();
 routes
     .add('/','index')
     .add('/hospitals','adminPage')
-    .add('/addhospitals','hospital');
+    .add('/addhospitals','hospital')
+    .add('/discharge','discharge');
 
 module.exports = routes;
